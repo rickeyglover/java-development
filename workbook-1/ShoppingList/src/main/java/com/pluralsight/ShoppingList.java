a56427d433058ea688eda8688beab66239519ba3
rickeyglover <rickeygloverw@gmail.com>
2023-10-04 11:01:05 -0400

    completed ShoppingList app

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args)
+    {
+        System.out.println("Headphones");
+        System.out.println("Chips");
+        System.out.println("Deodorant");
+        System.out.println("BodyWash");
+        System.out.println("Brush");
+        System.out.println("Cookies");
+        System.out.println("Apples");
+        System.out.println("Bananas");
+        System.out.println("Grapes");
+        System.out.println("Blueberries");
+    }
+
+}
